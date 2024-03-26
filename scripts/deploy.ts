a63d97fe0f012ee4ab92ab1d05ac392af94ffcb8
Yash Agrawal <yash@devprotocol.xyz>
2024-03-26 15:53:24 +0530

    feat: add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,16 @@
-import { ethers } from 'hardhat'
+import { ethers, run } from 'hardhat'
 
 async function main() {
 	console.log('Starting deploy script on sbt-tokens...')
 
 	const [signer] = await ethers.getSigners()
 	console.log('Signer is: ', signer.address)
+	console.log()
 
+	// @TODO: modify this when deploying...
+	const minterUpdater = signer.address
+	// @TODO: modify this when deploying...
+	const minters = [signer.address]
 	// @TODO: modify this address when deploying...
 	const proxyAdmin = '0x2e8fCbd8d3968252f1FC427Ff06928343B650bc3'
 
@@ -14,6 +19,10 @@ async function main() {
 	const sbtFactory = await ethers.getContractFactory('SBT')
 	const sbtImplementation = await sbtFactory.deploy()
 	await sbtImplementation.deployed()
+	console.log(
+		` - SBT implementation deployed at address:${sbtImplementation.address}`
+	)
+	console.log()
 
 	// >>> Deploy SBTProxy >>>
 	console.log('Deploying SBT proxy...')
@@ -24,15 +33,29 @@ async function main() {
 		ethers.utils.arrayify('0x')
 	)
 	await sbtProxyInstance.deployed()
+	console.log(` - SBT proxy deployed at address:${sbtProxyInstance.address}`)
+	console.log()
 
 	// >>> Initialize SBT Proxy >>>
 	console.log('Initializing SBT proxy...')
 	const sbtProxy = sbtFactory.attach(sbtProxyInstance.address)
-	await sbtProxy.functions.initialize(signer.address, [signer.address])
+	await sbtProxy.functions.initialize(minterUpdater, minters)
 
-	console.log('Signer:', signer.address)
-	console.log('SBTProxy deployed to:', sbtProxyInstance.address)
-	console.log('SBTImplementation deployed to:', sbtImplementation.address)
+	// >>> Verify SBTImplementation code >>>
+	await run(`verify:verify`, {
+		address: sbtImplementation.address,
+		constructorArguments: [],
+	})
+
+	// >>> Verify SBTProxy code >>>
+	await run(`verify:verify`, {
+		address: sbtProxyInstance.address,
+		constructorArguments: [
+			sbtImplementation.address,
+			proxyAdmin,
+			ethers.utils.arrayify('0x'),
+		],
+	})
 }
 
 main()
