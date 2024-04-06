e48b38c49d0a92197c99e98f6c414deb7ca43b25
aggre <hiroyuki.aggre@gmail.com>
2024-04-06 22:38:53 +0900

    add args.ts

---
+++ scripts/args.ts
@@ -0,0 +1,12 @@
+/**
+ * This file contains the constructor arguments for verifying the contracts.
+ * @example
+ * ```bash
+ * yarn hardhat verify --network polygonMainnet --constructor-args scripts/args.ts --contract contracts/SBTProxy.sol:SBTProxy <CONTRACT_ADDRESS>
+ * ```
+ */
+export default [
+	'0x748161365d3b6F1032efe0066c8594728974cD14',
+	'0x02021C49DdBdf5349dFc42bE4D4a8e01Bc476342',
+	'0x',
+]
