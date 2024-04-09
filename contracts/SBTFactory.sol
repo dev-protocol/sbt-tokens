39ece70737d162a42a723f8b88c3cad272103755
Yash Agrawal <yash@devprotocol.xyz>
2024-04-09 12:50:39 +0530

    fix: use dynamic sbt name, symbol in initialize

--- contracts/SBTFactory.sol
+++ contracts/SBTFactory.sol
@@ -21,6 +21,8 @@ contract SBTFactory is ISBTFactory, OwnableUpgradeable {
 	}
 
 	function makeNewSBT(
+		string memory sbtName,
+		string memory sbtSymbol,
 		address proxyAdmin,
 		bytes memory proxyCallData,
 		address minterUpdater,
@@ -52,7 +54,7 @@ contract SBTFactory is ISBTFactory, OwnableUpgradeable {
 		sbtProxyMapping[identifier] = address(proxy);
 
 		// Initialize the proxy.
-		SBT(proxy).initialize(minterUpdater, minters);
+		SBT(proxy).initialize(sbtName, sbtSymbol, minterUpdater, minters);
 
 		return address(proxy);
 	}
